a09cf759c5c75bdf50ac8ee24c91f35c73419ddb
mohandehab112 <Honda977@>
2023-08-20 01:46:42 +0200

    first commit

---
+++ src/main/java/com/example/hosting/HostingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostingApplication.class, args);
+	}
+
+}
